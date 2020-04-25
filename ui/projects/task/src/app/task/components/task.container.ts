2f4a4cd40b15ff9b0807fb0e280ddf896db1edb9
blazinskip <blazinski.patryk@gmail.com>
2020-04-25 12:34:27 +0200

    feat: 🎸 connect task ui with task backend

--- ui/projects/task/src/app/task/components/task.container.ts
+++ ui/projects/task/src/app/task/components/task.container.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { startWith, switchMap } from 'rxjs/operators';
+import { ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-task-container',
   template: `
     <app-task>
       <app-task-form (taskAdded)="addTask($event)"></app-task-form>
-      <app-tasks [tasks]="tasks"></app-tasks>
+      <app-tasks [tasks]="(tasks$ | async)"></app-tasks>
     </app-task>
   `
 })
 export class TaskContainer {
-  tasks: { name: string }[] = [];
+  private readonly fetchTasksSubject = new ReplaySubject(1);
+
+  tasks$ = this.fetchTasksSubject
+    .asObservable()
+    .pipe(
+      switchMap(() => this.taskService.findAll$().pipe(startWith([]))),
+    );
+
+  constructor(private readonly taskService: TaskService) {
+    this.fetchTasksSubject.next();
+  }
 
   addTask(task: { name: string }) {
-    this.tasks = [...this.tasks, task];
+    this.taskService.create$(task)
+      .subscribe(this.fetchTasksSubject);
   }
 }
