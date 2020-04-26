5164708e7f3745b234ec6057f9e66ecc4acf84df
blazinskip <blazinski.patryk@gmail.com>
2020-04-26 13:50:16 +0200

    feat: 🎸 Dockerfile for ui

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,9 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Task } from './task';
 
 @Injectable()
 export class TaskService {
+  private readonly logger = new Logger(TaskService.name);
+
   constructor(
     @Inject('TASK_MODEL')
     private taskModel: Model<Task>,
@@ -11,6 +13,7 @@ export class TaskService {
 
   async create(createTask: { name: string }): Promise<Task> {
     const createdTask = new this.taskModel(createTask);
+    this.logger.log(`Created task with name <${createdTask.name}>`)
     return await createdTask.save();
   }
 
